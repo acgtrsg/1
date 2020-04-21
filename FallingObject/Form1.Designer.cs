5fa23264d002d82ff06f382d146f83335bd53fad
acgtrsg <64101398+acgtrsg@users.noreply.github.com>
2020-04-21 22:15:49 +0300

    Add files via upload

---
+++ FallingObject/Form1.Designer.cs
@@ -0,0 +1,203 @@
+﻿namespace FallingObject
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.axis1 = new myUCLib.Axis();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // axis1
+            // 
+            this.axis1.axis_bkcolor = System.Drawing.Color.White;
+            this.axis1.axis_color = System.Drawing.Color.Gray;
+            this.axis1.Axis_Type = ((byte)(1));
+            this.axis1.E_x = 0;
+            this.axis1.E_y = 0;
+            this.axis1.Location = new System.Drawing.Point(12, 231);
+            this.axis1.Name = "axis1";
+            this.axis1.Pix_color = System.Drawing.Color.Black;
+            this.axis1.Pix_Size = 2F;
+            this.axis1.Pix_type = ((byte)(1));
+            this.axis1.Size = new System.Drawing.Size(612, 325);
+            this.axis1.TabIndex = 0;
+            this.axis1.x_Base = 1F;
+            this.axis1.x_Name = "X";
+            this.axis1.y_Base = 1F;
+            this.axis1.y_Name = "Y";
+            this.axis1.Load += new System.EventHandler(this.axis1_Load);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(211, 69);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(211, 101);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 2;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(211, 132);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 3;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(40, 69);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(103, 36);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(163, 76);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(33, 13);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "угол ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(177, 108);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(19, 13);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "V0";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(177, 135);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(13, 13);
+            this.label6.TabIndex = 15;
+            this.label6.Text = "y";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(38, 114);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(105, 38);
+            this.button2.TabIndex = 16;
+            this.button2.Text = "Stop";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(197, 208);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(177, 17);
+            this.checkBox1.TabIndex = 18;
+            this.checkBox1.Text = "RealCondition(В вязкой среде)";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // timer2
+            // 
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(511, 196);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(74, 29);
+            this.button3.TabIndex = 21;
+            this.button3.Text = "Clean";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(913, 568);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.axis1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private myUCLib.Axis axis1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ FallingObject/Form1.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FallingObject
+{
+    public partial class Form1 : Form
+    {
+
+        float x=0, y, vx, vy, vx0, vy0, v, alpha, t=0 , y0, ax, ay, x0 ;
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            axis1.ClearDin();
+            axis1.ClearPic(); 
+            x = 0; y = 0; t = 0; 
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            timer1.Enabled = false;
+            timer2.Enabled = false;
+            x = 0; y = 0; t = 0; 
+        }
+
+          public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+           float dt = 0.01f;
+            t = t + dt;          
+            vx = vx - t * vx * vx * 0.001f;
+            x = x + vx * t;
+            vy = vy + t * 9.81f;
+            y = y - vy * t;
+            if (y <= 0)
+            {
+                timer2.Enabled = false;
+                x = 0; y = 0; t = 0;
+            }
+            axis1.PixDraw(x, y, Color.Green, 1);
+            axis1.StatToPic();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+           try {
+            y = (float)(Convert.ToDouble(textBox3.Text));
+               }catch
+            {
+                y = 0; 
+            }
+            alpha = (float)Convert.ToDouble(textBox1.Text);
+            v = (float)(Convert.ToDouble(textBox2.Text));
+            vx = (float)(v * Math.Cos(alpha * Math.PI / 180));
+            vy = (float)(v * Math.Sin(-alpha * Math.PI / 180));  
+            if (checkBox1.Checked == false)
+            {
+                timer1.Interval = 1;
+                timer1.Enabled = true;
+            }
+            else
+            {
+                timer2.Interval = 1;
+                timer2.Enabled = true;
+            }
+        }
+        private void axis1_Load(object sender, EventArgs e)
+        {
+            axis1.Axis_Type = 3;
+            axis1.x_Name = "X";
+            axis1.y_Name = "Y";
+            axis1.x_Base = 1000;
+            axis1.y_Base = 1000;
+            axis1.Pix_Size = 0.008f;
+            axis1.AxisDraw();
+            axis1.PixDraw(x, y, Color.Black, 1);
+            axis1.StatToPic();
+        }
+        private void timer1_Tick(object sender,  EventArgs e)
+        {
+
+            t = t + 0.1f;
+            x = vx * t;
+            try
+            {
+ y = (float)((float)(Convert.ToDouble(textBox3.Text)) - vy * t - 9.82 * t * t / 2);
+            }
+            catch
+            {
+                y = (float)(- vy * t - 9.82 * t * t / 2);
+            }
+            if (y <= 0)
+            {
+                timer1.Enabled = false;
+                x = 0; y = 0; t = 0;
+            }
+
+            axis1.PixDraw(x, y, Color.Red, 1);
+            axis1.StatToPic();
+        }
+
+ 
+      
+
+
+    }
+}
